297aab8f05db81f2814d01537e3c1ce7d4f0a322
AzuGhost <110129525+AzuGhost@users.noreply.github.com>
2023-10-19 20:45:10 -0500

    corregido

--- src/entities/carro.entity.ts
+++ src/entities/carro.entity.ts
@@ -1,18 +1,51 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn} from "typeorm";
 
-@Entity('cars')
+@Entity('cars', {schema: 'inventory'})
 export class CarEntity {
 
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column('varchar', {name: 'model', nullable: false})
+  @CreateDateColumn({ 
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP'
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP'
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamp',
+    nullable: true
+  })
+  deletedAt: Date;
+
+  @Column('varchar', {
+    name: 'model',
+    nullable: false,
+    comment: 'Modelo del auto'
+  })
   model: string;
 
-  @Column('numeric', {name: 'horsePower', nullable: false})
+  @Column('numeric', {
+    name: 'horse_power', 
+    nullable: false,
+    comment: 'Caballos de fuerza'
+  })
   horsePower: number;
 
-  @Column('int', {name: 'doors', nullable: false})
+  @Column('int', {
+    name: 'doors',
+    nullable: false,
+    comment: 'Número de puertas'
+  })
   doors: number;
 
 }
--- src/entities/cereales.entity.ts
+++ src/entities/cereales.entity.ts
@@ -1,18 +1,50 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";  
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn} from "typeorm";
 
-@Entity('cereals')
+@Entity('cereals', {schema: 'products'})
 export class CerealEntity {
 
-  @PrimaryGeneratedColumn('uuid') 
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column('varchar', {name: 'name', nullable: false})
-  name: string;
-  
-  @Column('varchar', {name: 'brand', nullable: false})
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP'
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at', 
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP'
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamp', 
+    nullable: true
+  })
+  deletedAt: Date;
+
+  @Column('varchar', {
+    name: 'name',
+    nullable: false,
+    comment: 'Nombre del cereal'
+  })
+  name: string;  
+
+  @Column('varchar', {
+    name: 'brand',
+    nullable: false,
+    comment: 'Marca del cereal'
+  })
   brand: string;
 
-  @Column('text', {name: 'ingredients'})
+  @Column('text', {
+    name: 'ingredients', 
+    comment: 'Ingredientes del cereal'
+  })
   ingredients: string;
 
 }
